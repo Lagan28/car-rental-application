d800a779b33f9d499bd66b495dc6462d734a57c4
Lagan Gupta <53617100+Lagan28@users.noreply.github.com>
2022-01-20 18:12:53 +0530

    basic-template-design

---
+++ car-rental-app/src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+import {Homepage} from "./app/containers/Homepage";
+import styled from 'styled-components';
+import tw from 'twin.macro';
+
+const AppContainer = styled.div`
+  ${tw`
+    w-full
+    h-full
+    flex
+    flex-col
+    
+  `}
+`;
+
+function App() {
+  return <AppContainer>
+    <Homepage />
+  </AppContainer>
+}
+
+export default App;
